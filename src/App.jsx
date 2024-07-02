0dbb4f024cce9c105908b13217bcaebfb658120b
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-07-02 15:04:55 +0100

    layout elements added, functionality has not

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { DemonstratingProps } from "./components/demo/DemonstratingProps";
+import React from 'react'
+import QRCode from "qrcode"
 
 function App() {
+
+   const [generatedQRCode, setGeneratedQRCode] = React.useState(false)
+
+   function handleClick(event){
+        setGeneratedQRCode(event.target.uRLInput)
+        console.log(event)
+   }
+
     return (
         <div>
-            <h1>App component</h1>
-            <DemonstratingProps />
+            <h1>Qr Code Generator</h1>
+            <form name='qRCodeForm'>
+                <input name='uRLInput' type="text" placeholder="https://youtube.com/" />
+            
+            <button onClick={handleClick}>Generate</button>
+            </form>
+            {!generatedQRCode && <p>enter a url and click Generate, to see code</p>}
+            <img></img>
+            
         </div>
     );
 }
